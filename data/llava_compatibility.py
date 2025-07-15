1c2f3a27a99c9e2140c8b1b2f9834a519a3cfeeb
anderson da rosa <andersonfsdaroa@gmail.com>
2025-07-15 00:16:32 -0300

    classification eval

--- data/llava_compatibility.py
+++ data/llava_compatibility.py
@@ -2,15 +2,15 @@ import json
 from tqdm import tqdm
 
 if __name__ == '__main__':
-    filename = "E:\\datasets\\mimic\\preprocess\\test_split.json"
+    filename = "E:\\datasets\\mimic\\preprocess\\train_split.json"
     data = json.load(open(filename, 'r'))
     new_data = []
     for sample in tqdm(data):
         new_sample = {'image': sample['image_name'], 'id': sample['id'], 'conversations': []}
         # new_sample['conversations'].append({'from': 'system', 'value': '\nYou are a medical AI assistant specialized in interpreting chest radiographs.'})
-        new_sample['conversations'].append({'from': 'human', 'value': '\ndescribe the findings in <image>'})
+        new_sample['conversations'].append({'from': 'human', 'value': 'describe the findings in <image>'})
         new_sample['conversations'].append({'from': 'gpt', 'value': sample['findings']})
         new_data.append(new_sample)
 
-    json.dump(new_data, open('E:\\datasets\\mimic\\preprocess\\test_split_llava.json', 'w'), indent=2)
+    json.dump(new_data, open('E:\\datasets\\mimic\\preprocess\\train_split_llava.json', 'w'), indent=2)
 
--- eval/classification_eval.py
+++ eval/classification_eval.py
@@ -24,6 +24,7 @@ if __name__ == '__main__':
     parser.add_argument('--annotation_file', type=str, required=True, help='annotation file')
     parser.add_argument('--model_path', type=str, required=True, help='path to saved model weights')
     parser.add_argument('--model_base_path', type=str, required=True, help='path to base model')
+    parser.add_argument('--dim', type=int, default=256*3072)
     args = parser.parse_args()
 
     device = torch.device("cuda" if torch.cuda.is_available() else "")
@@ -31,11 +32,11 @@ if __name__ == '__main__':
     loader = data.get_loader(args.batch_size)
     config = json.load(open(os.path.join(args.model_path, 'experiment.json'), 'r'))
 
-    classifier = MultiClassifier(mimic_classifier_list, 256*3072, config['output_classes']).to(device)
-    encoder, preprocess = get_encoder(args.model_base_path)
+    classifier = MultiClassifier(mimic_classifier_list, args.dim, config['output_classes']).to(device)
+    encoder, preprocess = get_encoder(args.model_base_path, args.dim)
     if config['lora']:
         encoder = lora(encoder, config['lora_rank'], config['lora_alpha'], config['lora_dropout'])
-        encoder.base_model.model.load_state_dict(torch.load(os.path.join(args.model_path, 'backbone_checkpoint.pt'),
+        encoder.load_state_dict(torch.load(os.path.join(args.model_path, 'backbone_checkpoint.pt'),
                                                         map_location=device)['model_state_dict'])
     else:
         encoder.load_state_dict(torch.load(os.path.join(args.model_path, 'backbone_checkpoint.pt'),
@@ -46,9 +47,9 @@ if __name__ == '__main__':
 
     predictions = {}
     gt = {}
-    for label in mimic_classifier_list:
-        predictions[label] = []
-        gt[label] = []
+    for name in mimic_classifier_list:
+        predictions[name] = []
+        gt[name] = []
 
     for i, batch in tqdm(enumerate(loader), total=len(loader)):
         with torch.no_grad():
@@ -65,14 +66,18 @@ if __name__ == '__main__':
                 gt[name] += labels[name]
 
     target_names = ['negative', 'positive', 'uncertain', 'not present']
-    result_dict = {'condition': [], 'macro f1-score': [], 'macro precision': [], 'macro recall': []}
-
+    result_dict = []
     for name in mimic_classifier_list:
         report = classification_report(gt[name], predictions[name], labels=range(len(target_names)), target_names=target_names, zero_division=0, output_dict=True)
-        result_dict['condition'].append(name)
-        result_dict['macro f1-score'].append(round(report['macro avg']['f1-score'], 5))
-        result_dict['macro recall'].append(round(report['macro avg']['recall'], 5))
-        result_dict['macro precision'].append(round(report['macro avg']['precision'], 5))
+        result = {}
+        # print(report)
+        result['condition'] = name
+        result['report'] = report
+        # result['accuracy'] = report['accuracy']
+        # result['macro_precision'] = report['macro avg']['precision']
+        # result['macro_recall'] = report['macro avg']['recall']
+        # result['macro_f1'] = report['macro avg']['f1-score']
+        result_dict.append(result)
 
     output_file = os.path.join(args.model_path, 'classification_eval.json')
     json.dump(result_dict, open(output_file, 'w'), indent=2)
